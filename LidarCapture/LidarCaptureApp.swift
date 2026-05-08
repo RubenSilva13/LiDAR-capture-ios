23696a73802e7a3ddbf2f0ac90e06a446776f30f
Ruben Marcelo Baptista Silva <a54949@alunos.ipb.pt>
2026-05-08 13:26:26 +0100

    Initial Commit

---
+++ LidarCapture/LidarCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidarCaptureApp.swift
+//  LidarCapture
+//
+//  Created by Ruben Marcelo Baptista Silva on 08/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LidarCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
